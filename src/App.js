35d2247664a6dd80b8fd37b6f024bfb78e1132ff
Dilven <mateusiakkrystian@gmail.com>
2018-07-31 11:30:58 +0200

    Basic authorization for slack

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import './App.css';
 import Main from './containers/Main';
+import LoadingPage from './containers/LoadingPage/LoadingPage'
 
 class App extends Component {
   render() {
     return (
-    <div>
-      <Main />
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/" component={Main} />
+        <Route path="/authorization" component={LoadingPage} />
+      </div>
+    </Router>
     );
   }
 }
