5166df945d4a89a4e3a4acd76546c8801283a952
Dilven <mateusiakkrystian@gmail.com>
2018-08-01 11:08:16 +0200

    added select channel

--- src/components/SlackMessage/SlackMessage.js
+++ src/components/SlackMessage/SlackMessage.js
@@ -2,9 +2,8 @@ import React from 'react'
 import { List } from 'semantic-ui-react';
 
 export default ({ data , activeTask }) => {
-  console.log(activeTask)
   return (
-    <List.Item key={data.ts} className={activeTask === data.id ? 'list-item__active' : ''}>
+    <List.Item className={activeTask === data.id ? 'list-item__active' : ''}>
       <List.Icon name='github' size='large' verticalAlign='middle' />
       <List.Content>
         <List.Header as='a'>{ data.name || ''}</List.Header>
