5ef54b2ffeac601887c27f29268392401ea58627
Artur <artur.kaluza@almservices.pl>
2018-08-02 10:54:43 +0200

    Get Data from JIRA

--- src/components/Jira/Jira.js
+++ src/components/Jira/Jira.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './Jira.css';
 
 class Jira extends Component {
@@ -6,24 +7,43 @@ class Jira extends Component {
     super(props);
 
     this.state = {
+      projects: [],
       activeTask: false
     }
 
     this.checkActiveTask = this.checkActiveTask.bind(this);
+    this.getProjectList = this.getProjectList.bind(this);
   }
   
   componentDidMount() {
     this.checkActiveTask();
+    this.getProjectList();
+
   }
 
   componentWillReceiveProps(newProps) {
     this.setState({activeTask: newProps.handleActiveTask})
-}
+  }
 
   checkActiveTask() {
     this.setState({activeTask: this.props.handleActiveTask})
     
   }
+
+  getProjectList() {
+    axios.get('https://supportdesk.almservices.pl/rest/api/2/project/', {
+      headers: {
+        Authorization: 'Basic YXJ0dXIua2FsdXphOmFzZHp4YzEx'
+      }
+    }).then(res => {
+      const projectArray = []
+
+      projectArray.push(res.data[2], res.data[8], res.data[9]);
+      this.setState({projects: projectArray})
+     
+    })
+  }
+   
 
   render() {
     return (
@@ -33,10 +53,22 @@ class Jira extends Component {
         </div>
   
         <div className='Jira__list'>
-          <div className={this.state.activeTask === 1 ? 'Jira__item list-item__active' : 'Jira__item'} onClick={() => this.props.jiraTask(1)}>Task 1</div>
-          <div className={this.state.activeTask === 2 ? 'Jira__item list-item__active' : 'Jira__item'}  onClick={() => this.props.jiraTask(2)}>Task 2</div>
-          <div className={this.state.activeTask === 3 ? 'Jira__item list-item__active' : 'Jira__item'} onClick={() => this.props.jiraTask(3)}>Task 3</div>
+          {this.state.projects.map((item, index) => <div 
+            className={this.state.activeTask === (index + 1) ? 'Jira__item list-item__active' : 'Jira__item'}
+            key={index}
+            onClick={() => this.props.jiraTask(index + 1)}
+            > 
+            {item.name}
+
+          </div>
+          )}
         </div>
+
+      {/* //   <div className='Jira__list'>
+      //     <div className={this.state.activeTask === 1 ? 'Jira__item list-item__active' : 'Jira__item'} onClick={() => this.props.jiraTask(1)}>Task 1</div>
+      //     <div className={this.state.activeTask === 2 ? 'Jira__item list-item__active' : 'Jira__item'}  onClick={() => this.props.jiraTask(2)}>Task 2</div>
+      //     <div className={this.state.activeTask === 3 ? 'Jira__item list-item__active' : 'Jira__item'} onClick={() => this.props.jiraTask(3)}>Task 3</div>
+      //   </div> */}
       </div>
     )
   }  
